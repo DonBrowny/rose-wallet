0c53c1bbefd49a46929df74a666f3ac80ff3f414
DonBrowny <kishore13ask@gmail.com>
2025-10-07 22:42:59 +0530

    add sms review component

---
+++ src/components/ui/sms-review-item/sms-review-item.tsx
@@ -0,0 +1,66 @@
+import { HighlightedSMS } from '@/components/ui/highlighted-sms/highlighted-sms'
+import { Input } from '@/components/ui/input/input'
+import { Text } from '@/components/ui/text/text'
+import React from 'react'
+import { View } from 'react-native'
+import Animated, { FadeInUp, FadeOutDown } from 'react-native-reanimated'
+import { styles } from './sms-review-item.styles'
+
+interface SmsReviewItemProps {
+  id: string
+  bankName: string
+  date: number
+  messageBody: string
+  merchant: string
+  amount: number
+}
+
+export function SmsReviewItem({ id, bankName, date, messageBody, merchant, amount }: SmsReviewItemProps) {
+  return (
+    <Animated.View
+      key={id}
+      entering={FadeInUp.duration(300)}
+      exiting={FadeOutDown.duration(300)}
+      style={styles.container}
+    >
+      <View style={styles.headerRow}>
+        <View style={styles.bankRow}>
+          <Text
+            variant='pSm'
+            color='muted'
+          >
+            Bank:
+          </Text>
+          <Text
+            variant='pMdBold'
+            color='muted'
+          >
+            {' '}
+            {bankName}
+          </Text>
+        </View>
+        <Text
+          variant='pSm'
+          color='muted'
+        >
+          {new Date(date).toLocaleString()}
+        </Text>
+      </View>
+      <HighlightedSMS
+        text={messageBody}
+        merchant={merchant}
+        amount={amount}
+      />
+      <Input
+        label='Amount'
+        defaultValue={String(amount)}
+        keyboardType='numeric'
+        inputMode='numeric'
+      />
+      <Input
+        label='Merchant'
+        defaultValue={merchant}
+      />
+    </Animated.View>
+  )
+}
