ce29a177800aef03fe20e0843bec5fdea6021d32
DonBrowny <kishore13ask@gmail.com>
2025-11-24 14:10:43 +0530

    version update

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,12 @@
 import { HomeScreen } from '@/screens/home-screen/home-screen'
 // TODO: Add the plugin expo-drizzle-studio-plugin back in when it is compatible with the latest version of Expo.
-// import { DB_NAME } from '@/types/constants'
-// import { useDrizzleStudio } from 'expo-drizzle-studio-plugin'
-// import * as SQLite from 'expo-sqlite'
+import { DB_NAME } from '@/types/constants'
+import { useDrizzleStudio } from 'expo-drizzle-studio-plugin'
+import * as SQLite from 'expo-sqlite'
 
-// const db = SQLite.openDatabaseSync(DB_NAME)
+const db = SQLite.openDatabaseSync(DB_NAME)
 
 export default function Index() {
-  // useDrizzleStudio(db)
+  useDrizzleStudio(db)
   return <HomeScreen />
 }
