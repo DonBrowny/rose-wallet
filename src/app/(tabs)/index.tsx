f0856dbcaba5ba146a6fbab996fb8ea6ea83ce5a
DonBrowny <kishore13ask@gmail.com>
2025-11-24 16:11:21 +0530

    move drizzle studio to dev

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,15 @@
 import { HomeScreen } from '@/screens/home-screen/home-screen'
 import { DB_NAME } from '@/types/constants'
-import { useDrizzleStudio } from 'expo-drizzle-studio-plugin'
 import * as SQLite from 'expo-sqlite'
 
 const db = SQLite.openDatabaseSync(DB_NAME)
 
 export default function Index() {
-  useDrizzleStudio(db)
+  if (__DEV__) {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    const { useDrizzleStudio } = require('expo-drizzle-studio-plugin')
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useDrizzleStudio(db)
+  }
   return <HomeScreen />
 }
